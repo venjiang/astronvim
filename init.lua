b1df352619a96aa8532e4bd8c63794b92b180d91
venjiang <venjiang@gmail.com>
2023-03-24 17:27:45 +0800

    update

--- init.lua
+++ init.lua
@@ -1,16 +1,48 @@
 return {
-  -- options
+  opt = {
+    timeoutlen = 100, -- Shorten key timeout length a little bit for which-key
+    timeout = 300,
+  },
   options = {
     g = {
       mapleader = ",", -- sets vim.g.mapleader
     },
   },
   -- mappings
-  mappings = {},
+  mappings = {
+    n = {
+      ["<leader>a"] = { "ggVG<cr>", desc = "Select all" },
+      ["<leader><cr>"] = { "<cmd>:nohl<cr>", desc = "No highlight" },
+      ["<C-t>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+      ["<C-w>"] = { "<cmd>w!<cr>", desc = "Save" },
+      ["<leader>r"] = { function() require("telescope.builtin").oldfiles() end, desc = "Find history" },
+      ["L"] = {
+        function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+        desc = "Next buffer",
+      },
+      ["H"] = {
+        function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+        desc = "Previous buffer",
+      },
+    },
+    t = {
+      ["<C-t>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+    },
+  },
   -- lsp
   lsp = {},
   -- plugins
   plugins = {
+    {
+      "goolord/alpha-nvim",
+      opts = function(_, opts)      -- override the options using lazy.nvim
+        opts.section.header.val = { -- change the header section value
+          "",
+          "",
+        }
+        -- opts.section.buttons.val = {}
+      end,
+    },
     {
       "nvim-neo-tree/neo-tree.nvim",
       config = {
