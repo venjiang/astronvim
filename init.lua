f1e7d2d157bafa05b4cf36e103e560d3292bac82
venjiang <venjiang@gmail.com>
2023-12-22 17:59:28 +0800

    plugin: nvim-lastplace

--- init.lua
+++ init.lua
@@ -219,7 +219,23 @@ return {
       event = "VeryLazy",
       config = function() require("nvim-surround").setup {} end,
     },
-
+    -- last place
+    {
+      "ethanholz/nvim-lastplace",
+      event = "BufRead",
+      config = function()
+        require("nvim-lastplace").setup {
+          lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+          lastplace_ignore_filetype = {
+            "gitcommit",
+            "gitrebase",
+            "svn",
+            "hgcommit",
+          },
+          lastplace_open_folds = true,
+        }
+      end,
+    },
     -- others
   },
 }
